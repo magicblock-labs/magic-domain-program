d80b7d39dc5ede7f76baea93f27a70e455c0d46f
Babur Makhmudov <bmuddha13@gmail.com>
2025-05-29 19:59:26 +0400

    fix(sync): adjust rent balance during ER record resize

--- src/processors/sync.rs
+++ src/processors/sync.rs
@@ -1,7 +1,11 @@
 use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
+    program::invoke,
     program_error::ProgramError,
+    rent::Rent,
+    system_instruction,
+    sysvar::Sysvar,
 };
 
 use crate::{instructions::sync::SyncInstruction, state::record::ErRecord, ID};
@@ -13,6 +17,7 @@ pub fn process_sync_record<'a>(
 ) -> Result<(), ProgramError> {
     let payer = next_account_info(&mut accounts)?;
     let pda_account = next_account_info(&mut accounts)?;
+    let system_program = next_account_info(&mut accounts)?;
 
     if *pda_account.owner != ID {
         return Err(ProgramError::InvalidAccountOwner);
@@ -54,7 +59,25 @@ pub fn process_sync_record<'a>(
     if let Some(load_average) = ix.load_average().take() {
         record.set_load_average(load_average);
     }
-
+    let new_size = borsh::object_length(&record)?;
+    let old_size = data.len();
+    if old_size == new_size {
+        return Ok(record.serialize(&mut *data)?);
+    };
+    drop(data);
+    let rent_new = Rent::get()?.minimum_balance(new_size);
+    let rent_old = Rent::get()?.minimum_balance(old_size);
+    if rent_new > rent_old {
+        invoke(
+            &system_instruction::transfer(payer.key, pda_account.key, rent_new - rent_old),
+            &[payer.clone(), pda_account.clone(), system_program.clone()],
+        )?;
+    } else {
+        **pda_account.try_borrow_mut_lamports()? -= rent_old - rent_new;
+        **payer.try_borrow_mut_lamports()? += rent_old - rent_new;
+    }
+    pda_account.realloc(new_size, false)?;
+    data = pda_account.try_borrow_mut_data()?;
     record.serialize(&mut *data)?;
 
     Ok(())
